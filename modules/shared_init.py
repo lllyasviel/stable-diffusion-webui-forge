b00e93e878368cda3048700fb7a6fdf82f87c2db
lllyasviel <lyuminzhang@outlook.com>
2024-01-24 11:20:34 -0800

    Update shared_init.py

--- modules/shared_init.py
+++ modules/shared_init.py
@@ -23,6 +23,10 @@ def initialize():
     except FileNotFoundError:
         pass
 
+    from modules import devices
+    shared.device = devices.device
+    shared.weight_load_location = None if cmd_opts.lowram else "cpu"
+
     from modules import shared_state
     shared.state = shared_state.State()
 

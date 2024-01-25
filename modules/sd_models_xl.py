95918b4f8241874c0df0ff8accb819b2c4607687
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 12:23:06 -0800

    Update sd_models_xl.py

--- modules/sd_models_xl.py
+++ modules/sd_models_xl.py
@@ -22,7 +22,7 @@ def get_learned_conditioning(self: sgm.models.diffusion.DiffusionEngine, batch: 
     is_negative_prompt = getattr(batch, 'is_negative_prompt', False)
     aesthetic_score = shared.opts.sdxl_refiner_low_aesthetic_score if is_negative_prompt else shared.opts.sdxl_refiner_high_aesthetic_score
 
-    devices_args = dict(device=self.clip_patcher.current_device, dtype=model_management.text_encoder_dtype())
+    devices_args = dict(device=self.forge_objects.clip.patcher.current_device, dtype=model_management.text_encoder_dtype())
 
     sdxl_conds = {
         "txt": batch,

db7f16d1cc02bf55ef2b2125c6fa617adad8141c
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 15:41:34 -0800

    Create forge_freeu.py

---
+++ extensions-builtin/sd_forge_freeu/scripts/forge_freeu.py
@@ -0,0 +1,23 @@
+import gradio as gr
+from modules import scripts
+
+
+class FreeUForForge(scripts.Script):
+    def title(self):
+        return "FreeU Integrated"
+
+    def ui(self, is_img2img):
+        with gr.Accordion(open=False, label=self.title()):
+            freeu_enabled = gr.Checkbox(label='Enabled', value=False)
+            freeu_b1 = gr.Slider(label='B1', minimum=0, maximum=2, step=0.01, value=1.01)
+            freeu_b2 = gr.Slider(label='B2', minimum=0, maximum=2, step=0.01, value=1.02)
+            freeu_s1 = gr.Slider(label='S1', minimum=0, maximum=4, step=0.01, value=0.99)
+            freeu_s2 = gr.Slider(label='S2', minimum=0, maximum=4, step=0.01, value=0.95)
+
+        return freeu_enabled, freeu_b1, freeu_b2, freeu_s1, freeu_s2
+
+    def process(self, p, *args, **kwargs):
+        return
+
+    def process_batch(self, p, *args, **kwargs):
+        return

a3b4f9df29c669f0693dba354b054bc42c0661a0
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 08:03:28 -0800

    Update sd_models.py

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -523,7 +523,6 @@ class SdModelData:
             sd_vae.base_vae = getattr(v, "base_vae", None)
             sd_vae.loaded_vae_file = getattr(v, "loaded_vae_file", None)
             sd_vae.checkpoint_info = v.sd_checkpoint_info
-        self.loaded_sd_models = [self.sd_model]
 
 
 model_data = SdModelData()

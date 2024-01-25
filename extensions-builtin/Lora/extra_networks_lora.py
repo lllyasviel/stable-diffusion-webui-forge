005b8d3869725f07fec81613780184c75308ea9e
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 11:26:41 -0800

    Revert "Update extra_networks_lora.py"
    This reverts commit a815b3db548e44d0f778624c70028260efd3ed56.

--- extensions-builtin/Lora/extra_networks_lora.py
+++ extensions-builtin/Lora/extra_networks_lora.py
@@ -10,7 +10,13 @@ class ExtraNetworkLora(extra_networks.ExtraNetwork):
         """mapping of network names to the number of errors the network had during operation"""
 
     def activate(self, p, params_list):
+        additional = shared.opts.sd_lora
+
         self.errors.clear()
+
+        if additional != "None" and additional in networks.available_networks and not any(x for x in params_list if x.items[0] == additional):
+            p.all_prompts = [x + f"<lora:{additional}:{shared.opts.extra_networks_default_multiplier}>" for x in p.all_prompts]
+            params_list.append(extra_networks.ExtraNetworkParams(items=[additional, shared.opts.extra_networks_default_multiplier]))
 
         names = []
         te_multipliers = []

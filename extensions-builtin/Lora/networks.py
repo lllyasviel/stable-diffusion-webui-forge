09da75583d65504c43bddb96635ebb08fc068ada
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 12:13:05 -0800

    Update networks.py

--- extensions-builtin/Lora/networks.py
+++ extensions-builtin/Lora/networks.py
@@ -135,8 +135,8 @@ def load_networks(names, te_multipliers=None, unet_multipliers=None, dyn_dims=No
         return
 
     current_sd.current_lora_hash = compiled_lora_targets_hash
-    current_sd.unet_patcher, current_sd.clip_patcher = \
-        current_sd.unet_patcher_original, current_sd.clip_patcher_original
+    current_sd.forge_objects.unet = current_sd.forge_objects.unet_original
+    current_sd.forge_objects.clip = current_sd.forge_objects.clip_original
 
     for filename, strength_model, strength_clip in compiled_lora_targets:
         if filename in lora_state_dict_cache:
@@ -148,8 +148,8 @@ def load_networks(names, te_multipliers=None, unet_multipliers=None, dyn_dims=No
             lora_sd = load_torch_file(filename, safe_load=True)
             lora_state_dict_cache[filename] = lora_sd
 
-        current_sd.unet_patcher, current_sd.clip_patcher = load_lora_for_models(
-            current_sd.unet_patcher, current_sd.clip_patcher, lora_sd, strength_model, strength_clip)
+        current_sd.forge_objects.unet, current_sd.forge_objects.clip = load_lora_for_models(
+            current_sd.forge_objects.unet, current_sd.forge_objects.clip, lora_sd, strength_model, strength_clip)
 
     return
 

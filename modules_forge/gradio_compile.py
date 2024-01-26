e76ab46ec9d5f584cf4b97e10bf0400df711a735
lllyasviel <lyuminzhang@outlook.com>
2024-01-25 22:28:02 -0800

    Update gradio_compile.py

--- modules_forge/gradio_compile.py
+++ modules_forge/gradio_compile.py
@@ -5,9 +5,10 @@ def gradio_compile(items, prefix):
             t, d = v
             if t == 'INT':
                 name = (prefix + '_' + k).replace(' ', '_').lower()
+                title = name.replace('_', ' ').capitalize()
                 default = int(d['default'])
                 min = int(d['min'])
                 max = int(d['max'])
                 step = int(d.get('step', 1))
-                print(f'{name} = gr.Slider(label=\'{name}\', minimum={min}, maximum={max}, step={step}, value={default})')
+                print(f'{name} = gr.Slider(label=\'{title}\', minimum={min}, maximum={max}, step={step}, value={default})')
     return

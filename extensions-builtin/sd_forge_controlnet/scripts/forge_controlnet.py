374ff2ca166ff05d67a71114386d4d9714cab21c
lllyasviel <lyuminzhang@outlook.com>
2024-01-27 10:15:56 -0800

    Update forge_controlnet.py

--- extensions-builtin/sd_forge_controlnet/scripts/forge_controlnet.py
+++ extensions-builtin/sd_forge_controlnet/scripts/forge_controlnet.py
@@ -14,19 +14,21 @@ class ControlNetForge(scripts.Script):
         return scripts.AlwaysVisible
 
     def build_unit_ui(self):
-        return
+        return []
 
     def ui(self, *args, **kwargs):
+        all_ui_components = []
+
         with gr.Accordion(open=False, label=self.title()):
             unit_count = gr.Slider(label='ControlNet Units', minimum=0, maximum=MAX_UNITS, step=1, value=DEFAULT_UNITS)
             unit_accordions = []
             for i in range(16):
                 with gr.Accordion(open=True, label=f'ControlNet Unit {i+1}',
                                   visible=i < DEFAULT_UNITS) as unit_accordion:
-                    self.build_unit_ui()
+                    all_ui_components += self.build_unit_ui()
                 unit_accordions.append(unit_accordion)
 
             unit_count.change(lambda c: [gr.update(visible=i < c) for i in range(MAX_UNITS)],
                               inputs=unit_count, outputs=unit_accordions, show_progress=False, queue=False)
 
-        return [unit_count]
+        return all_ui_components

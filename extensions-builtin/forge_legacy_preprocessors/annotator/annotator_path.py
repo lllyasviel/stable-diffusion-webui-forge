c8b0489346ebfb65f8c703a2098b160640aa2c8a
lllyasviel <lyuminzhang@outlook.com>
2024-01-28 15:15:46 -0800

    i

--- extensions-builtin/forge_legacy_preprocessors/annotator/annotator_path.py
+++ extensions-builtin/forge_legacy_preprocessors/annotator/annotator_path.py
@@ -1,7 +1,9 @@
 import os
 from modules import shared
+from modules_forge.shared import preprocessor_dir
 
-models_path = shared.opts.data.get('control_net_modules_path', None)
+
+models_path = preprocessor_dir
 if not models_path:
     models_path = getattr(shared.cmd_opts, 'controlnet_annotator_models_path', None)
 if not models_path:
@@ -10,13 +12,10 @@ if not models_path:
 if not os.path.isabs(models_path):
     models_path = os.path.join(shared.data_path, models_path)
 
-clip_vision_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'clip_vision')
-# clip vision is always inside controlnet "extensions\sd-webui-controlnet"
-# and any problem can be solved by removing controlnet and reinstall
-
+clip_vision_path = os.path.join(preprocessor_dir, 'clip_vision')
 models_path = os.path.realpath(models_path)
+
 os.makedirs(models_path, exist_ok=True)
+os.makedirs(clip_vision_path, exist_ok=True)
+
 print(f'ControlNet preprocessor location: {models_path}')
-# Make sure that the default location is inside controlnet "extensions\sd-webui-controlnet"
-# so that any problem can be solved by removing controlnet and reinstall
-# if users do not change configs on their own (otherwise users will know what is wrong)

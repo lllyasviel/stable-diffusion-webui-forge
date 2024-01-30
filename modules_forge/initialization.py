cb34e10956b5a07a6c71bb84f1036c09ac9b4ca7
lllyasviel <lyuminzhang@outlook.com>
2024-01-29 22:52:11 -0800

    Update initialization.py

--- modules_forge/initialization.py
+++ modules_forge/initialization.py
@@ -1,5 +1,18 @@
+import sys
+
 
 def initialize_forge():
+    bad_list = ['--lowvram', '--medvram', '--medvram-sdxl']
+
+    for bad in bad_list:
+        if bad in sys.argv:
+            print(f'Arg {bad} is removed in Forge.')
+            print(f'Now memory management is fully automatic and you do not need any command flags.')
+            print(f'Please just remove this flag.')
+            print(f'In extreme cases, if you want to force previous lowvram/medvram behaviors, '
+                  f'please use --always-offload-from-vram')
+            exit(0)
+
     from ldm_patched.modules import args_parser
 
     args_parser.args, _ = args_parser.parser.parse_known_args()

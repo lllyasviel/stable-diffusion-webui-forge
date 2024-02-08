70564d670472fb18712c638ae67770e6dec9aadd
continue revolution <continuerevolution@gmail.com>
2024-02-08 00:32:15 -0600

    revert

--- ldm_patched/modules/samplers.py
+++ ldm_patched/modules/samplers.py
@@ -245,8 +245,7 @@ def calc_cond_uncond_batch(model, cond, uncond, x_in, timestep, model_options):
                 p.transformer_options = transformer_options
                 p = p.previous_controlnet
             control_cond = c.copy()  # get_control may change items in this dict, so we need to copy it
-            cn_function_wrapper = model_options.get("cn_function_wrapper", None)
-            c['control'] = control.get_control(input_x, timestep_, control_cond, len(cond_or_uncond), cn_function_wrapper)
+            c['control'] = control.get_control(input_x, timestep_, control_cond, len(cond_or_uncond))
 
         if 'model_function_wrapper' in model_options:
             output = model_options['model_function_wrapper'](model.apply_model, {"input": input_x, "timestep": timestep_, "c": c, "cond_or_uncond": cond_or_uncond}).chunk(batch_chunks)

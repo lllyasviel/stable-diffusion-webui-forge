847d4515057d7087dbffd180b1d39e5b36d7d458
Yuki Shindo <shinshin86@users.noreply.github.com>
2024-02-09 09:47:42 +0900

    Fix command line arguments format in webui-user.bat (#135)
    * fix command line arguments format in webui-user.bat
    
    * fix embeddings dir path

--- webui-user.bat
+++ webui-user.bat
@@ -9,10 +9,10 @@ set COMMANDLINE_ARGS=
 @REM set A1111_HOME=Your A1111 checkout dir
 @REM
 @REM set VENV_DIR=%A1111_HOME%/venv
-@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS%^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks^
-@REM  --embeddings-dir %A1111_HOME%/models/embeddings^
+@REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
+@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%/embeddings ^
 @REM  --lora-dir %A1111_HOME%/models/Lora
 
 call webui.bat

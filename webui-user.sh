673e0ecfd7e2aee6dad182d5abd507eb9cc2d35d
shanbenkazuki <shanbenkazuki15@gmail.com>
2024-03-12 02:06:38 +0900

    設定ファイルをMac用に書き換え

--- webui-user.sh
+++ webui-user.sh
@@ -1,48 +1,17 @@
 #!/bin/bash
-#########################################################
-# Uncomment and change the variables below to your need:#
-#########################################################
+####################################################################
+#                          macOS defaults                          #
+# Please modify webui-user.sh to change these instead of this file #
+####################################################################
 
-# Install directory without trailing slash
-#install_dir="/home/$(whoami)"
+if [[ -x "$(command -v python3.10)" ]]
+then
+    python_cmd="python3.10"
+fi
 
-# Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+export install_dir="$HOME"
+export COMMANDLINE_ARGS="--skip-torch-cuda-test --upcast-sampling --no-half-vae --use-cpu interrogate"
+export TORCH_COMMAND="pip install torch==2.1.0 torchvision==0.16.0"
+export PYTORCH_ENABLE_MPS_FALLBACK=1
 
-# Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
-
-# python3 executable
-#python_cmd="python3"
-
-# git executable
-#export GIT="git"
-
-# python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
-
-# script to launch to start the app
-#export LAUNCH_SCRIPT="launch.py"
-
-# install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
-
-# Requirements file to use for stable-diffusion-webui
-#export REQS_FILE="requirements_versions.txt"
-
-# Fixed git repos
-#export K_DIFFUSION_PACKAGE=""
-#export GFPGAN_PACKAGE=""
-
-# Fixed git commits
-#export STABLE_DIFFUSION_COMMIT_HASH=""
-#export CODEFORMER_COMMIT_HASH=""
-#export BLIP_COMMIT_HASH=""
-
-# Uncomment to enable accelerated launch
-#export ACCELERATE="True"
-
-# Uncomment to disable TCMalloc
-#export NO_TCMALLOC="True"
-
-###########################################
+####################################################################

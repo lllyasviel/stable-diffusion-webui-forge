34a913428b1ac4a35e4ba1e91fb0e5adb4370ea9
PegasusSpectra <126330607+PegasusSpectra@users.noreply.github.com>
2024-06-01 13:01:03 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -8,11 +8,11 @@ set COMMANDLINE_ARGS=
 @REM Uncomment following code to reference an existing A1111 checkout.
 @REM set A1111_HOME=Your A1111 checkout dir
 @REM
-@REM set VENV_DIR=%A1111_HOME%/venv
+@REM set VENV_DIR=%A1111_HOME%\\venv
 @REM set COMMANDLINE_ARGS=%COMMANDLINE_ARGS% ^
-@REM  --ckpt-dir %A1111_HOME%/models/Stable-diffusion ^
-@REM  --hypernetwork-dir %A1111_HOME%/models/hypernetworks ^
-@REM  --embeddings-dir %A1111_HOME%/embeddings ^
-@REM  --lora-dir %A1111_HOME%/models/Lora
+@REM  --ckpt-dir %A1111_HOME%\\models\\Stable-diffusion ^
+@REM  --hypernetwork-dir %A1111_HOME%\\models\\hypernetworks ^
+@REM  --embeddings-dir %A1111_HOME%\\embeddings ^
+@REM  --lora-dir %A1111_HOME%\\models\\Lora
 
 call webui.bat

c3e17f05f343208ec2a6af816a664d773dca3278
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-07-29 09:22:44 -0600

    fix #880

--- modules/ui_loadsave.py
+++ modules/ui_loadsave.py
@@ -53,7 +53,10 @@ class UiLoadsave:
                 field = 'open'
 
             if saved_value is None:
-                self.ui_settings[key] = getattr(obj, field)
+                value_in_gradio = getattr(obj, field)
+                if isinstance(obj, gr.Textbox) and field == 'value' and value_in_gradio is None:
+                    value_in_gradio = ''  # Gradio 4 fix: https://github.com/lllyasviel/stable-diffusion-webui-forge/issues/880
+                self.ui_settings[key] = value_in_gradio
             elif condition and not condition(saved_value):
                 pass
             else:

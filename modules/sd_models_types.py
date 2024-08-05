98ed477c3acb656f9e3b7220273812f5f8cbd250
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-05 03:52:14 -0700

    Revert "Update sd_models_types.py"
    This reverts commit 471d07caddb423d74fdaa5f04f6f71eb313c65f2.

--- modules/sd_models_types.py
+++ modules/sd_models_types.py
@@ -1,3 +1,4 @@
+from ldm.models.diffusion.ddpm import LatentDiffusion
 from typing import TYPE_CHECKING
 
 
@@ -5,7 +6,7 @@ if TYPE_CHECKING:
     from modules.sd_models import CheckpointInfo
 
 
-class WebuiSdModel:
+class WebuiSdModel(LatentDiffusion):
     """This class is not actually instantinated, but its fields are created and fieeld by webui"""
 
     lowvram: bool

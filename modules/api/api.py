77a4980f08538522b1437c7490ede817c1ea0973
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-05 04:29:23 -0700

    remove everything related to training

--- modules/api/api.py
+++ modules/api/api.py
@@ -21,8 +21,7 @@ from modules import sd_samplers, deepbooru, sd_hijack, images, scripts, ui, post
 from modules.api import models
 from modules.shared import opts
 from modules.processing import StableDiffusionProcessingTxt2Img, StableDiffusionProcessingImg2Img, process_images
-from modules.textual_inversion.textual_inversion import create_embedding, train_embedding
-from modules.hypernetworks.hypernetwork import create_hypernetwork, train_hypernetwork
+from modules.textual_inversion.textual_inversion import create_embedding
 from PIL import PngImagePlugin
 from modules.realesrgan_model import get_realesrgan_models
 from modules import devices
@@ -235,8 +234,6 @@ class Api:
         self.add_api_route("/sdapi/v1/refresh-vae", self.refresh_vae, methods=["POST"])
         self.add_api_route("/sdapi/v1/create/embedding", self.create_embedding, methods=["POST"], response_model=models.CreateResponse)
         self.add_api_route("/sdapi/v1/create/hypernetwork", self.create_hypernetwork, methods=["POST"], response_model=models.CreateResponse)
-        self.add_api_route("/sdapi/v1/train/embedding", self.train_embedding, methods=["POST"], response_model=models.TrainResponse)
-        self.add_api_route("/sdapi/v1/train/hypernetwork", self.train_hypernetwork, methods=["POST"], response_model=models.TrainResponse)
         self.add_api_route("/sdapi/v1/memory", self.get_memory, methods=["GET"], response_model=models.MemoryResponse)
         self.add_api_route("/sdapi/v1/unload-checkpoint", self.unloadapi, methods=["POST"])
         self.add_api_route("/sdapi/v1/reload-checkpoint", self.reloadapi, methods=["POST"])
@@ -798,52 +795,6 @@ class Api:
             return models.CreateResponse(info=f"create hypernetwork filename: {filename}")
         except AssertionError as e:
             return models.TrainResponse(info=f"create hypernetwork error: {e}")
-        finally:
-            shared.state.end()
-
-    def train_embedding(self, args: dict):
-        try:
-            shared.state.begin(job="train_embedding")
-            apply_optimizations = shared.opts.training_xattention_optimizations
-            error = None
-            filename = ''
-            if not apply_optimizations:
-                sd_hijack.undo_optimizations()
-            try:
-                embedding, filename = train_embedding(**args) # can take a long time to complete
-            except Exception as e:
-                error = e
-            finally:
-                if not apply_optimizations:
-                    sd_hijack.apply_optimizations()
-            return models.TrainResponse(info=f"train embedding complete: filename: {filename} error: {error}")
-        except Exception as msg:
-            return models.TrainResponse(info=f"train embedding error: {msg}")
-        finally:
-            shared.state.end()
-
-    def train_hypernetwork(self, args: dict):
-        try:
-            shared.state.begin(job="train_hypernetwork")
-            shared.loaded_hypernetworks = []
-            apply_optimizations = shared.opts.training_xattention_optimizations
-            error = None
-            filename = ''
-            if not apply_optimizations:
-                sd_hijack.undo_optimizations()
-            try:
-                hypernetwork, filename = train_hypernetwork(**args)
-            except Exception as e:
-                error = e
-            finally:
-                shared.sd_model.cond_stage_model.to(devices.device)
-                shared.sd_model.first_stage_model.to(devices.device)
-                if not apply_optimizations:
-                    sd_hijack.apply_optimizations()
-                shared.state.end()
-            return models.TrainResponse(info=f"train embedding complete: filename: {filename} error: {error}")
-        except Exception as exc:
-            return models.TrainResponse(info=f"train embedding error: {exc}")
         finally:
             shared.state.end()
 

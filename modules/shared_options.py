5f908c2bf3b0dc447eb821a1f255b5742c78458c
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-06 15:11:40 -0700

    add forge opts section

--- modules/shared_options.py
+++ modules/shared_options.py
@@ -5,6 +5,8 @@ from modules import localization, ui_components, shared_items, shared, interroga
 from modules.paths_internal import models_path, script_path, data_path, sd_configs_path, sd_default_config, sd_model_file, default_sd_model_file, extensions_dir, extensions_builtin_dir, default_output_dir  # noqa: F401
 from modules.shared_cmd_options import cmd_opts
 from modules.options import options_section, OptionInfo, OptionHTML, categories
+from modules_forge import shared_options as forge_shared_options
+
 
 options_templates = {}
 hide_dirs = shared.hide_dirs
@@ -422,3 +424,5 @@ options_templates.update(options_section((None, "Hidden options"), {
     "restore_config_state_file": OptionInfo("", "Config state file to restore from, under 'config-states/' folder"),
     "sd_checkpoint_hash": OptionInfo("", "SHA256 hash of the current checkpoint"),
 }))
+
+forge_shared_options.register(options_templates, options_section, OptionInfo)

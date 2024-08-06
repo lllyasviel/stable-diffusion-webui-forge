78e25a8baecfa3ec0f3a9219c3bb84b1054e658b
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-06 16:49:53 -0700

    Update shared_options.py

--- modules_forge/shared_options.py
+++ modules_forge/shared_options.py
@@ -1,5 +1,5 @@
 
 def register(options_templates, options_section, OptionInfo):
     options_templates.update(options_section((None, "Forge Hidden options"), {
-        "forge_unet_storage_dtype": OptionInfo('Automatic'),
+        "forge_unet_storage_dtype": OptionInfo('None'),
     }))

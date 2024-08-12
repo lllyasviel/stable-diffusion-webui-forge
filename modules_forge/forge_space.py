40a29ac7a2fec3db03880c8efd9540eeeb560283
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-11 22:26:16 -0700

    Update forge_space.py

--- modules_forge/forge_space.py
+++ modules_forge/forge_space.py
@@ -1,6 +1,16 @@
 import gradio as gr
+from threading import Thread
+
+
+def open_another():
+    def greet(name):
+        return "Hello " + name + "!"
+
+    demo = gr.Interface(fn=greet, inputs="text", outputs="text")
+    demo.launch(inbrowser=True)
 
 
 def main_ui():
-    gr.Markdown('Hello')
+    btn = gr.Button('Hello')
+    btn.click(lambda: Thread(open_another).start())
     return

ce94d4153a3219235abd8f34a0907eeccf09c49c
DenOfEquity <166248528+DenOfEquity@users.noreply.github.com>
2024-08-17 18:38:32 +0100

    Update processing.py
    add distilled_cfg_scale to params that invalidate cond cache

--- modules/processing.py
+++ modules/processing.py
@@ -413,6 +413,7 @@ class StableDiffusionProcessing:
 
         return (
             required_prompts,
+            self.distilled_cfg_scale,
             steps,
             hires_steps,
             use_old_scheduling,

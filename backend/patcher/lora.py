9973d5dc09758eb8c4fa818347b3c9e26ba3dd69
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-16 21:13:09 -0700

    better prints

--- backend/patcher/lora.py
+++ backend/patcher/lora.py
@@ -303,7 +303,7 @@ class LoraLoader:
 
         # Patch
 
-        for key, current_patches in (tqdm(self.patches.items(), desc='Patching LoRAs') if len(self.patches) > 0 else self.patches):
+        for key, current_patches in (tqdm(self.patches.items(), desc=f'Patching LoRAs for {type(self.model).__name__}') if len(self.patches) > 0 else self.patches):
             try:
                 parent_layer, weight = utils.get_attr_with_parent(self.model, key)
                 assert isinstance(weight, torch.nn.Parameter)

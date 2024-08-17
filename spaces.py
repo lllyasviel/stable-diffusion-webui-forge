cbf94695cf09cdcce1619d3c782cc70e518ca60f
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-17 07:28:31 -0700

    Update spaces.py

--- spaces.py
+++ spaces.py
@@ -3,9 +3,43 @@ from modules_forge.initialization import initialize_forge
 initialize_forge()
 
 import os
+import torch
 import inspect
 
 from backend import memory_management
+
+
+class GPUObject:
+    def __init__(self):
+        self.module_list = []
+
+    def __enter__(self):
+        self.original_init = torch.nn.Module.__init__
+        self.original_to = torch.nn.Module.to
+
+        def patched_init(module, *args, **kwargs):
+            self.module_list.append(module)
+            return self.original_init(module, *args, **kwargs)
+
+        def patched_to(module, *args, **kwargs):
+            self.module_list.append(module)
+            return self.original_to(module, *args, **kwargs)
+
+        torch.nn.Module.__init__ = patched_init
+        torch.nn.Module.to = patched_to
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        torch.nn.Module.__init__ = self.original_init
+        torch.nn.Module.to = self.original_to
+        self.module_list = set(self.module_list)
+        self.to(device=torch.device('cpu'))
+        return
+
+    def to(self, *args, **kwargs):
+        for module in self.module_list:
+            module.to(*args, **kwargs)
+        return
 
 
 def GPU(func, models=[]):

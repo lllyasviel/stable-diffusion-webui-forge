f05205bd29b296c319f8feb08f2240f27e9fa6fb
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-17 05:39:42 -0700

    Create spaces.py

---
+++ spaces.py
@@ -0,0 +1,6 @@
+def GPU(func):
+    def wrapper():
+        print("Something is happening before the function is called.")
+        func()
+        print("Something is happening after the function is called.")
+    return wrapper

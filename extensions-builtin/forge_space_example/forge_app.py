72ab92f83e5a9e193726313c6d88ab435a61fb59
layerdiffusion <19834515+lllyasviel@users.noreply.github.com>
2024-08-18 00:12:53 -0700

    upload meta files

---
+++ extensions-builtin/forge_space_example/forge_app.py
@@ -0,0 +1,16 @@
+import spaces
+import gradio as gr
+
+
+def greet(name, intensity):
+    return "Hello, " + name + "!" * int(intensity)
+
+
+demo = gr.Interface(
+    fn=greet,
+    inputs=["text", "slider"],
+    outputs=["text"],
+)
+
+if __name__ == "__main__":
+    demo.launch()
--- modules_forge/config.py
+++ modules_forge/config.py
@@ -1,4 +1,5 @@
 always_disabled_extensions = [
     'sd-webui-controlnet',
-    'multidiffusion-upscaler-for-automatic1111'
+    'multidiffusion-upscaler-for-automatic1111',
+    'forge_space_example'
 ]

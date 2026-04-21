d39d02fa6e5e3ccc0bd2de34bf7e722697608651
sznapiGoat <f.sznapi@gmail.com>
2026-04-21 22:15:15 +0200

    Update webui-user.bat

--- webui-user.bat
+++ webui-user.bat
@@ -3,7 +3,7 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
+set COMMANDLINE_ARGS=--medvram-v2 --autolaunch --xformers
 
 @REM Uncomment following code to reference an existing A1111 checkout.
 @REM set A1111_HOME=Your A1111 checkout dir
